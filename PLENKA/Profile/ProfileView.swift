483bf7cb94230335740bcda6006f92c3770208e4
Nariman Vildanov <vildanovnr@icloud.com>
2023-07-13 14:03:57 +0300

    ProfileView

--- PLENKA/Profile/ProfileView.swift
+++ PLENKA/Profile/ProfileView.swift
@@ -5,138 +5,59 @@
 //  Created by Nariman on 12.07.2023.
 //
 
+import Foundation
 import SwiftUI
 
 struct ProfileView: View {
-    @State private var isShowingFollowers = false
-    @State private var isShowingFollowing = false
-    var name = ["1", "2", "2", "4", "5","1", "2", "3", "4", "5","1", "2", "3", "4", "5","1", "2", "3", "4", "5","1", "2", "3", "4", "5","1", "2", "3", "4", "5","1", "2", "3", "4", "5","1", "2", "3", "4", "5",]
-    
     var body: some View {
-        VStack {
-            ZStack {
+        ZStack {
+            VStack {
                 Image("UserPhoto")
                     .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 120, height: 120)
-                    .clipShape(Circle())
-                    .padding(.top, 16)
+                    .frame(width: 150.0, height: 150)
+                    .clipped()
+                    .scaledToFit()
+                    .cornerRadius(100)
+                    .padding(3)
                 
-                VStack {
-                    Spacer()
-                    HStack {
-                        Spacer()
-                        Button(action: {
-                        }) {
-                            Image(systemName: "camera")
-                                .font(.system(size: 24))
-                                .foregroundColor(.white)
-                                .padding(12)
-                                .background(Color.gray.opacity(0.8))
-                                .clipShape(Circle())
-                        }
-                    }
-                }
+                Text("UserName")
+                    .font(.system(size: 55.5, weight: .heavy))
+                Text("Hello! I am a beginner photographer, I will be glad if you like my content !!!")
+                    .font(.system(size: 20.5, weight: .regular))
+                    .multilineTextAlignment(.center)
+                    .lineLimit(2)
+                    .padding(.horizontal, 35)
             }
+            .padding(.bottom, 450)
             
-            Text("Adel Ahtyamov")
-                .font(.title)
-                .padding(.top, 8)
-            
-            Text("@ahtyam_10")
-                .font(.subheadline)
-                .foregroundColor(.gray)
-                .padding(.top, 4)
+            Divider()
+                .background(Color.black)
+                .padding(.bottom, 130)
             
             HStack {
-                Spacer()
-                
-                VStack {
-                    Text("Posts")
-                        .font(.headline)
-                    Text("100")
-                        .font(.subheadline)
+                VStack(alignment: .leading) {
+                    Text("post")
+                        .font(.system(size: 28.5, weight: .medium))
+                    Text("followers")
+                        .font(.system(size: 28.5, weight: .medium))
+                    Text("following")
+                        .font(.system(size: 28.5, weight: .medium))
                 }
-                
+                .padding(.leading, 30)
                 Spacer()
-                
-                VStack {
-                    Button(action: {
-                        self.isShowingFollowers.toggle()
-                    }) {
-                        Text("Followers")
-                            .font(.headline)
-                        Text("1K")
-                            .font(.subheadline)
-                    }
-                    .foregroundColor(.primary)
-                    .sheet(isPresented: $isShowingFollowers) {
-                        // Экран подписчиков
-                        Text("Followers")
-                    }
+                VStack(alignment: .center) {
+                    Text("10")
+                        .font(.system(size: 28.5, weight: .regular))
+                    Text("1k")
+                        .font(.system(size: 28.5, weight: .regular))
+                    Text("16")
+                        .font(.system(size: 28.5, weight: .regular))
                 }
-                
-                Spacer()
-                
-                VStack {
-                    Button(action: {
-                        self.isShowingFollowing.toggle()
-                    }) {
-                        Text("Following")
-                            .font(.headline)
-                        Text("500")
-                            .font(.subheadline)
-                    }
-                    .foregroundColor(.primary)
-                    .sheet(isPresented: $isShowingFollowing) {
-                        ScrollView {
-                            VStack {
-                                Text("Following:")
-                                    .font(.system(size: 23.5, weight: .bold))
-                                Divider()
-                            }
-                            ForEach(name, id: \.self) { name in
-                                VStack(alignment: .leading) {
-                                    HStack {
-                                        Image("UserPhoto")
-                                            .resizable()
-                                            .frame(width: 30, height: 30)
-                                            .cornerRadius(100)
-                                        Text(name)
-                                    }
-                                    Divider()
-                                }
-                            }
-                        }
-                        .padding(10)
-                    }
-                }
-                
-                Spacer()
+                .padding(.trailing, 30)
             }
-            .padding(.top, 16)
-            
-            Button(action: {
-                // Действия при нажатии на кнопку редактирования профиля
-            }) {
-                Text("Edit Profile")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.blue)
-                    .cornerRadius(8)
-                    .padding(.top, 16)
-            }
-            .padding(.horizontal, 16)
-            
-            Spacer()
+            Divider()
+                .background(Color.black)
+                .padding(.top, 130)
         }
-    }
-}
-
-struct ProfileView_Previews: PreviewProvider {
-    static var previews: some View {
-        ProfileView()
     }
 }
